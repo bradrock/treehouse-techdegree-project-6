4c3893a551aaf0267923f02079b0d0306173b641
Brad Rock <32444574+bradrock@users.noreply.github.com>
2020-05-14 01:37:42 -0700

    fix image paths, set up about page, set up 404 error handling

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('view engine', 'pug');
 
 app.use('/static', express.static('public'));
 
+
+
 app.get('/', (req, res) => {
 
     res.render('index', {"projects" : projects});
@@ -27,9 +29,23 @@ app.get('/about', (req, res) => {
 
 app.get('/project/:id', (req, res) => {
 
-    res.render('project', data.value[req.params.id]);
+    res.render('project', {"project" : projects[req.params.id]});
 
 });
 
+
+app.use((req, res, next) =>
+    {
+        const err = new Error('Page not found.')
+        err.status = 404;
+        next(err);
+    });
+
+app.use((err, req, res, next) =>
+    {
+        res.locals.error = err;
+        res.status(err.status);
+        res.render('error');
+    });
 
 app.listen(3000);
